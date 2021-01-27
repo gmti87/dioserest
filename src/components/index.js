57099ea4411f8f15ef70cf2219e5e515a5bde6b7
Gabriel Moreti <gmoreti87@gmail.com>
2021-01-27 10:59:57 -0300

    Projeto Restaurante

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as RestaurantCard } from './RestaurantCard';
+export { default as Modal } from './Modal';
+export { default as Map } from './Map';
+export { default as ImageSkeleton } from './ImageSkeleton';
+export { default as Loader } from './Loader';
